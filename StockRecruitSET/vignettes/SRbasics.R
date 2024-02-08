bf3cd196427c297b73eb89e8c9f542831cd9efbc
mollie brooks <mollieebrooks@gmail.com>
2024-02-08 10:16:43 +0100

    same as above, but docs

--- StockRecruitSET/vignettes/SRbasics.R
+++ StockRecruitSET/vignettes/SRbasics.R
@@ -1,72 +1,75 @@
 params <-
 list(EVAL = TRUE)
 
-## ----load_lib,message=FALSE----------------------------------------------
+## ----load_lib,message=FALSE---------------------------------------------------
 library(StockRecruitSET)
 library(TMB)
 knitr::opts_chunk$set(warning = FALSE)
+set.seed(1111)
 
-## ----chsdat--------------------------------------------------------------
+## ----chsdat-------------------------------------------------------------------
 S = simS(n=100, pars=c(60,200))
-g=1 #assume this for
+g=1 #assume this for 
 R = simR(S=S, shape="contHockey", pars=c(.5, 100, g), varlog=.1)
 plot(S,R)
 
-## ----chsfit, message=FALSE-----------------------------------------------
+## ----chsfit, message=FALSE----------------------------------------------------
 mod=fitSRCurve(S, R, shape="contHockey", g=g)
 sdr=sdreport(mod)
 summary(sdr)
 
-## ----chblim--------------------------------------------------------------
-calcBlim(S, R)
+## ----chblim-------------------------------------------------------------------
+calcBlim(S, R) #hockey-stick breakpoint
 
-## ----chsplot-------------------------------------------------------------
+#If the stock was spasmodic, we might use the
+# average  of lowest 3 SSBs that give large (above median) recruitment
+calcBlim(S, R, type=1, quant=0.5, nmin=3)  
+
+## ----chsplot------------------------------------------------------------------
 Srange=seq(min(S), max(S), length=100)
 Rfit=simR(S=Srange,
-		shape="contHockey",
+		shape="contHockey", 
 		pars=c(exp(mod$env$last.par[c('log_beta','log_delta')]), g)
 )
 plot(S, R)
 lines(Srange, Rfit, col=2, lwd=3)
 abline(v = calcBlim(S, R), lty=2)
 
-## ----rdat----------------------------------------------------------------
+## ----rdat---------------------------------------------------------------------
 S = simS(n=100, pars=c(1,200))
 R = simR(S=S, shape="Ricker", pars=c(exp(1), 1/100), varlog=0.1)
 plot(S,R)
 
-## ----rfit, message=FALSE-------------------------------------------------
+## ----rfit, message=FALSE------------------------------------------------------
 mod=fitSRCurve(S, R, shape="Ricker")
 sdr=sdreport(mod)
 summary(sdr)
 
-## ----rplot---------------------------------------------------------------
+## ----rplot--------------------------------------------------------------------
 Srange=seq(min(S), max(S), length=100)
 Rfit=simR(S=Srange,
-		shape="Ricker",
+		shape="Ricker", 
 		pars=c(exp(mod$env$last.par[c('log_a','log_b')]))
 )
 plot(S, R)
 lines(Srange, Rfit, col=2, lwd=3)
 
-## ----bhdat---------------------------------------------------------------
+## ----bhdat--------------------------------------------------------------------
 S = simS(n=100, pars=c(1,200))
 R = simR(S=S, shape="BevertonHolt", pars=c(1.5456836,  0.0108868), varlog=0.1)
 plot(S,R)
 
-## ----bhfit, message=FALSE------------------------------------------------
+## ----bhfit, message=FALSE-----------------------------------------------------
 mod=fitSRCurve(S, R, shape="BevertonHolt")
 sdr=sdreport(mod)
 summary(sdr)
 
-## ----bhplot--------------------------------------------------------------
+## ----bhplot-------------------------------------------------------------------
 Srange=seq(min(S), max(S), length=100)
 Rfit=simR(S=Srange,
-		shape="BevertonHolt",
+		shape="BevertonHolt", 
 		pars=c(exp(mod$env$last.par[c('log_a','log_b')]))
 )
 plot(S, R)
 lines(Srange, Rfit, col=2, lwd=3)
-
-calcBlim(S, R, type=1, quant=0.5, nmin=100)
 
